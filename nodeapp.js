93cbea0d1bfa364eb565876950e8439121c56f0b
ajmusto <36599966+ajmusto@users.noreply.github.com>
2018-04-25 10:47:52 +0530

    Update nodeapp.js

--- nodeapp.js
+++ nodeapp.js
@@ -3,7 +3,7 @@ var handleRequest = function (request, response)
 { 
    console.log('Received request for URL:'+ request.url);
    response.writeHead(200);
-   response.end('Hello AJ231');
+   response.end('Hello AJ');
  };
  
  var www = http.createServer(handleRequest);
